0658f27d57752f40495fa48e3dcc9c234dd78e08
kabbott33 <kabbottvt@gmail.com>
2024-04-05 23:51:35 -0700

    Adjusted accuracy values

--- Assets/C# Scripts/Shooting.cs
+++ Assets/C# Scripts/Shooting.cs
@@ -98,7 +98,8 @@ public class Shooting : MonoBehaviour
 
     private void Shoot()
     {
-        if ((Input.GetKeyDown(KeyCode.Mouse0)) && Time.time > nextFire && remainingBullets > 0 && !isReloading)
+        //&& remainingBullets > 0 && !isReloading
+        if ((Input.GetKey(KeyCode.Mouse0)) && Time.time > nextFire )
         {
             nextFire = Time.time + fireRate;
             fire.Play();
@@ -131,7 +132,7 @@ public class Shooting : MonoBehaviour
         }
     }
 
-    bool isMoving
+    public bool isMoving
     {
         get
         {
