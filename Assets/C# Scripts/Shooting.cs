f65c98b82684150040d362a969a09dc0bf32b785
kabbott33 <kabbottvt@gmail.com>
2024-04-05 22:39:22 -0700

    Added reload system

--- Assets/C# Scripts/Shooting.cs
+++ Assets/C# Scripts/Shooting.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using Unity.PlasticSCM.Editor.WebApi;
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class Shooting : MonoBehaviour
 {
@@ -15,6 +17,12 @@ public class Shooting : MonoBehaviour
     public float fireRate = 0.01f;
     public float nextFire = 0.0f;
     public GameObject bulletHole;
+    public TextMeshProUGUI ammoText;
+    public Slider reloadSlider;
+    public float reloadTime = 3.0f;
+    private int remainingBullets = 6;
+    private float reloadTimer = 0.0f;
+    public bool isReloading = false;
 
     //ADS positions
     public GameObject gun;
@@ -29,20 +37,28 @@ public class Shooting : MonoBehaviour
     public float maxInaccuracy;
     private float currentAccuracy;
     public bool isAiming;
-    // Start is called before the first frame update
+
     void Start()
     {
         fire = this.GetComponent<AudioSource>();
         if (fire == null) fire = gameObject.AddComponent<AudioSource>();
 
+        UpdateAmmoUI();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Shoot();
-        ADS();
-        Accuracy();
+        if (isReloading)
+        {
+            PerformReload();
+        }
+
+
+            Shoot();
+            ADS();
+            Accuracy();
+            Reload();
+
     }
 
     private void Accuracy()
@@ -51,7 +67,7 @@ public class Shooting : MonoBehaviour
         {
             currentAccuracy = Mathf.Lerp(currentAccuracy, minInaccuracy, Time.deltaTime * speed);
         }
-        else if ((isMoving)&&!(isAiming))
+        else if ((isMoving) && !(isAiming))
         {
             currentAccuracy = Mathf.Lerp(currentAccuracy, maxInaccuracy, Time.deltaTime * speed);
         }
@@ -60,35 +76,38 @@ public class Shooting : MonoBehaviour
             currentAccuracy = Mathf.Lerp(currentAccuracy, midInaccuracy, Time.deltaTime * speed);
         }
     }
+
     private void ADS()
     {
         if ((Input.GetKey(KeyCode.Mouse1)))
         {
-            gun.transform.position = Vector3.Lerp(gun.transform.position, adsTransform.position, speed*Time.deltaTime);
+            gun.transform.position = Vector3.Lerp(gun.transform.position, adsTransform.position, speed * Time.deltaTime);
             isAiming = true;
         }
         else
         {
-            gun.transform.position = Vector3.Lerp(gun.transform.position, hipFireTransform.position, speed*Time.deltaTime);
+            gun.transform.position = Vector3.Lerp(gun.transform.position, hipFireTransform.position, speed * Time.deltaTime);
             isAiming = false;
         }
 
     }
+
     private void Shoot()
     {
-            if ((Input.GetKeyDown(KeyCode.Mouse0)) && Time.time>nextFire)
+        if ((Input.GetKeyDown(KeyCode.Mouse0)) && Time.time > nextFire && remainingBullets > 0 && !isReloading)
         {
             nextFire = Time.time + fireRate;
             fire.Play();
+            remainingBullets--;
+
+            UpdateAmmoUI();
+
             Vector3 aim = this.transform.forward;
             aim = Quaternion.AngleAxis(Random.Range(0, currentAccuracy), UnityEngine.Vector3.up) * aim;
             aim = Quaternion.AngleAxis(Random.Range(0, 360), this.transform.forward) * aim;
             RaycastHit hit;
             if (Physics.Raycast(this.transform.position, aim, out hit, maxRange))
             {
-                //        if ((hit.collider.CompareTag("Head")) || hit.collider.CompareTag("Body"))
-
-                //        {
                 if (hit.collider.CompareTag("Head"))
                 {
                     hit.transform.parent.GetComponent<EnemyHealth>().Headshot(damage);
@@ -104,13 +123,10 @@ public class Shooting : MonoBehaviour
                     GameObject bH = Instantiate(bulletHole, hit.point, Quaternion.LookRotation(hit.normal));
                     bH.transform.position += bH.transform.forward / 1000;
                 }
-                //GameObject bH= Instantiate(bulletHole, hit.point + new Vector3(hit.normal.x * 0.01f, hit.normal.y * 0.01f, hit.normal.z * 0.01f), Quaternion.LookRotation(hit.normal));
-
-                //        }
             }
         }
-       
     }
+
     bool isMoving
     {
         get
@@ -119,8 +135,42 @@ public class Shooting : MonoBehaviour
                 return true;
             else
                 return false;
-
-
         }
     }
+
+    private void PerformReload()
+    {
+        reloadTimer -= Time.deltaTime;
+        reloadSlider.value = reloadTime - reloadTimer; // Update TMP slider value
+        if (reloadTimer <= 0)
+        {
+            remainingBullets = 6;
+            isReloading = false;
+            reloadTimer = 0;
+            reloadSlider.gameObject.SetActive(false); // Hide TMP slider when reload is complete
+            UpdateAmmoUI();
+        }
+    }
+
+    public void Reload()
+    {
+        // Method to reload the gun
+        if (Input.GetKeyDown(KeyCode.R) && remainingBullets < 6)
+        {
+            isReloading = true;
+            reloadTimer = reloadTime;
+            reloadSlider.gameObject.SetActive(true); // Show TMP slider when reload starts
+            reloadSlider.minValue = 0;
+            reloadSlider.maxValue = reloadTime;
+
+            UpdateAmmoUI();
+        }
+    }
+
+    private void UpdateAmmoUI()
+    {
+        // Update ammo count UI
+        ammoText.text = "Ammo: " + remainingBullets + "/6";
+    }
+
 }
