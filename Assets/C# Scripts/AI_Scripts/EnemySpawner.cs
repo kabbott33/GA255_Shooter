a533a78b048bc492343069806edf0f5fa3eb3714
kabbott33 <kabbottvt@gmail.com>
2024-04-25 19:54:35 -0700

    Environment adjustments / exterior enemy placement

--- Assets/C# Scripts/AI_Scripts/EnemySpawner.cs
+++ Assets/C# Scripts/AI_Scripts/EnemySpawner.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject[] enemies;
     void Start()
     {
-        
+       foreach (var enemy in enemies) 
+        {
+        }
     }
 
     // Update is called once per frame
